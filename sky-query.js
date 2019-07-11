32d4efb6b251d6358818d8d3354e337552bd629c
linsk1998 <h@linsk.us>
2019-07-12 07:28:14 +0800

    基本配置按照jQuery修改

--- sky-query.js
+++ sky-query.js
@@ -3981,14 +3981,16 @@ Sky.fn.outerHeight=function(){
 };
 
 
-Sky.overload([Sky.isString,Sky.isDocument],Sky,Sky.query);
-Sky.overload([Sky.isString,Sky.isElement],Sky,Sky.query);
+Sky.overload([Sky.isString],Sky,Sky.query);
 Sky.overload([Sky.isElement],Sky,Sky.ele);
 Sky.overload([Sky.isDocument],Sky,Sky.ele);
+
+Sky.overload([Sky.isString,Sky.isObject],Sky,Sky.create);
+
+Sky.overload([Sky.isString,Sky.isDocument],Sky,Sky.query);
+Sky.overload([Sky.isString,Sky.isElement],Sky,Sky.query);
 
 Sky.overload([Sky.isFunction],Sky,function(callback){
 	Sky.ready().then(callback);
 });
 
-Sky.overload([Sky.isString],Sky,Sky.query);
-Sky.overload([Sky.isString,Sky.isObject],Sky,Sky.create);
